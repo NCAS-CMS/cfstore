10c8a1c7c14e3896e749d66a79c15a532111834f
GeorgeOBrien <goobrien1@sheffield.ac.uk>
2022-06-21 11:51:49 +0100

    Changes to cfin tests - remote and local seperated

--- cfstore/tests/test_cfin.py
+++ cfstore/tests/test_cfin.py
@@ -18,31 +18,6 @@ from click.testing import CliRunner
     'TEST_RP_EXPECTED_DIR': 'subdir'
 })
 
-def setup_ssh():
-    rhost = os.getenv('TEST_RP_HOST', default='NONE')
-    rpath = os.getenv('TEST_RP_PATH', default='NONE')
-    ruser = os.getenv('TEST_RP_USER', default='NONE')
-    expected = os.getenv('TEST_RP_EXPECTED_DIR', default='NONE')
-    if rhost == 'NONE' or rpath == 'NONE' or ruser == 'NONE' or expected == 'NONE':
-        raise MissingTestEnvVar(
-            f'RemotePosix test requires TEST_RP_HOST, TEST_RP_PATH, TEST_RP_USER, TEST_RP_EXPECTED_DIR env variables')
-    else:
-        s = SSHlite(rhost, ruser)
-        assert s.isalive(), 'SSH test configuration does not work'
-    return rhost, rpath, ruser, expected
-
-class Test_ssh(unittest.TestCase):
-
-    def test_ssh(self):
-        """
-        test the remote path includes an expected subdirectory
-        """
-        rhost, rpath, ruser, expected = setup_ssh()
-        s = SSHlite(rhost, ruser)
-        dlist = s.globish(rpath, '*')
-        assert expected in dlist
-
-
 class Test_cfin(unittest.TestCase):
     """
     Test the cfin command line interface
@@ -80,22 +55,6 @@ class Test_cfin(unittest.TestCase):
             result = runner.invoke(incli, ['local', 'add', 'testloc', lpath, f'--description={dfile}','subcollections=False'])
             result = runner.invoke(cli, ['ls', '--collection=testloc'] )
             _check(self, result, 8)
-
-    def test_add_remote_posix(self):
-        """
-        This test requires you to have an ssh host and location set in environment variables.
-        They are mocked here in setup_ssh, you might need to do the same.
-        You also need to sort out what is in the test directory.
-        """
-        rhost, rpath, ruser, expected = setup_ssh()
-        runner = CliRunner()
-        with runner.isolated_filesystem():
-            dfile = _mysetup()
-            result = runner.invoke(incli, ['rp', 'setup', 'testrem', rhost, ruser])
-            result = runner.invoke(incli, ['rp', 'add', 'testrem', rpath, 'testrem_collection', f'--description={dfile}'])
-            result = runner.invoke(cli, ['ls', '--collection=testrem_collection'] )
-            # there are supposed to be three files in the test collection
-            _check(self, result, 3)
 
     def test_add_local_posix_subcollections(self):
         """
@@ -138,15 +97,6 @@ class Test_cfin(unittest.TestCase):
             result = runner.invoke(incli, ['local', 'add', 'testlocall', lpath, f'--description={dfile}','--subcollections=True', '--regexselect=^et'])
             result = runner.invoke(cli, ['ls', '--collection=testlocall'] )
             _check(self, result, 6)
-
-    def test_add_remote_posix_subcollections(self):
-        pass
-
-    def test_add_remote_posix_regex(self):
-        pass
-
-    def test_add_remote_posix_all_optional(self):
-        pass
 
 if __name__=="__main__":
     unittest.main()
---
+++ cfstore/tests/test_cfin_remote.py
@@ -0,0 +1,87 @@
+import unittest, os
+
+from unittest import mock
+
+from cfstore.cfdb import cli
+from cfstore.cfin import cli as incli
+from test_cmd import _check, _mysetup, MissingTestEnvVar
+from test_basic import _dummy
+from cfstore.plugins.ssh import SSHlite
+
+
+from click.testing import CliRunner
+
+@mock.patch.dict(os.environ, {
+    'TEST_RP_HOST': 'xfer1',
+    'TEST_RP_PATH': 'hiresgw/cftest',
+    'TEST_RP_USER': 'lawrence',
+    'TEST_RP_EXPECTED_DIR': 'subdir'
+})
+
+def setup_ssh():
+    rhost = os.getenv('TEST_RP_HOST', default='NONE')
+    rpath = os.getenv('TEST_RP_PATH', default='NONE')
+    ruser = os.getenv('TEST_RP_USER', default='NONE')
+    expected = os.getenv('TEST_RP_EXPECTED_DIR', default='NONE')
+    if rhost == 'NONE' or rpath == 'NONE' or ruser == 'NONE' or expected == 'NONE':
+        raise MissingTestEnvVar(
+            f'RemotePosix test requires TEST_RP_HOST, TEST_RP_PATH, TEST_RP_USER, TEST_RP_EXPECTED_DIR env variables')
+    else:
+        s = SSHlite(rhost, ruser)
+        assert s.isalive(), 'SSH test configuration does not work'
+    return rhost, rpath, ruser, expected
+
+class Test_ssh(unittest.TestCase):
+
+    def test_ssh(self):
+        """
+        test the remote path includes an expected subdirectory
+        """
+        rhost, rpath, ruser, expected = setup_ssh()
+        s = SSHlite(rhost, ruser)
+        dlist = s.globish(rpath, '*')
+        assert expected in dlist
+
+
+class Test_cfin(unittest.TestCase):
+    """
+    Test the cfin command line interface
+    """
+    
+    def test_create_location1(self):
+        """ Test creating location which we know doesn't exist """
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            _mysetup()
+            result = runner.invoke(incli, ['rp', 'setup', 'a_location', 'host_does_not_exist', 'user'])
+
+    def test_create_location2(self):
+        """ Test creating location which we know does exist """
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            _mysetup()
+            result = runner.invoke(incli, ['rp', 'setup', 'location1', 'host_does_not_exist','user'])
+            # this should raise a ValueError given we already have this location
+            # it looks like it does, but somehow the test environment is removing it ...
+            result = runner.invoke(incli, ['rp', 'setup', 'location1', 'host_does_not_exist', 'user'])
+            assert result.exit_code == 1
+            assert str(result.exception).find('already exists in') != -1
+
+    def test_add_remote_posix(self):
+        """
+        This test requires you to have an ssh host and location set in environment variables.
+        They are mocked here in setup_ssh, you might need to do the same.
+        You also need to sort out what is in the test directory.
+        """
+        rhost, rpath, ruser, expected = setup_ssh()
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            dfile = _mysetup()
+            result = runner.invoke(incli, ['rp', 'setup', 'testrem', rhost, ruser])
+            result = runner.invoke(incli, ['rp', 'add', 'testrem', rpath, 'testrem_collection', f'--description={dfile}'])
+            result = runner.invoke(cli, ['ls', '--collection=testrem_collection'] )
+            # there are supposed to be three files in the test collection
+            _check(self, result, 3)
+
+if __name__=="__main__":
+    unittest.main()
