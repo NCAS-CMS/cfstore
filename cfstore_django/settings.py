3d88fd28a7451aa71eb0cd449cdbff4cfe51cd71
georgeoobrienNCAS <george.o'brien@ncas.ac.uk>
2023-07-03 10:35:31 +0100

    Trusted origins includes jasmin host

--- cfstore_django/settings.py
+++ cfstore_django/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['http://192.171.169.43']
