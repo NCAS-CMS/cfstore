5e774b02d37a4b7c0decfea8da14e07b97af4a14
georgeoobrienNCAS <george.o'brien@ncas.ac.uk>
2023-08-31 15:49:16 +0100

    browse search functionality implemented

--- cfstoreviewer/forms.py
+++ cfstoreviewer/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 
-from cfstore import db
-
+from cfstore.config import CFSconfig
 
 class VariableSearchForm(forms.Form):
     variablename = forms.CharField(label="Variable Search", max_length=100)
 
+class VariableBrowseForm(forms.Form):
+    my_object = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+    )
 
 class CollectionSearchForm(forms.Form):
     collection_searchname = forms.CharField(label="Collection Search", max_length=100)
@@ -13,11 +16,3 @@ class CollectionSearchForm(forms.Form):
 
 class SaveAsCollectionForm(forms.Form):
     collectionname = forms.CharField(label="Save as Collection", max_length=100)
-
-
-class FrequencyForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        brands = set(db.Variables.objects.all()._proxied.values)
-        for brand in brands:
-            self.fields[f"{brand}"] = forms.BooleanField(label=f"{brand}")
