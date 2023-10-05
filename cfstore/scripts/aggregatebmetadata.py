e3222cf77c9aae05df1a3c4814993d6de9905dcf
georgeoobrienNCAS <george.o'brien@ncas.ac.uk>
2023-10-05 14:09:53 +0100

    aggregate is now a seperate variable

--- cfstore/scripts/aggregatebmetadata.py
+++ cfstore/scripts/aggregatebmetadata.py
@@ -19,18 +19,22 @@ if __name__ == "__main__":
     # Reads the fields from the file with cf
     # Alternatively cfdm can be used as such:
     #   cff = cfdm.read(filename)
+
+    # If you want to configure the aggregation change this variable
+    aggregate = {
+        # "relaxed_units": True, (not needed if we're only looking at NetCDF files)
+        "relaxed_identities": True,
+        "exclude": False,
+        "concatenate": False,
+        "cells": cf.climatology_cells(),
+        "contiguous": True,
+    }
+
     cff = cf.read(
         "{{fileinput}}",
         ignore_read_error=True,
         fmt="NETCDF",
-        aggregate={
-            #            "relaxed_units": True, (not needed if we're only looking at NetCDF files)
-            "relaxed_identities": True,
-            "exclude": False,
-            "concatenate": False,
-            "cells": cf.climatology_cells(),
-            "contiguous": True,
-        },
+        aggregate=aggregate,
         recursive=True,
         chunks=None,
     )
