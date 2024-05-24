96d0bec7ee65f4d516f70d257241f40ce7d4b228
georgeoobrienNCAS <george.o'brien@ncas.ac.uk>
2024-05-24 17:46:22 +0100

    Canari file location fixed for latest db changes

--- cfstore/scripts/updatecanarilocations.py
+++ cfstore/scripts/updatecanarilocations.py
@@ -5,22 +5,20 @@ import numpy as np
 from netCDF4 import Dataset
 
 growncfa = Dataset(
-    "/home/george/Documents/cfs/cfstore/cfstore/json/19500101T0000Z_i.cfa",
+    "/home/george/Documents/cfs/cfstore/cfstore/json/CANARI_2_cw342_atmos.nc",
     "a",
     format="NETCDF4",
 )
 
-replacementfiles = {"${unavailable}cn134i_999_mon_20141101-20141201.nc": "${tarpe}"}
+replacementfiles = {"${tape}cw342_2_6hr_pt__195310-195310.nc": "${disc}"}
 
 
-def back_prop(a, i, s, c):
+def back_prop(a, i, s):
     if i == 0:
-        varval[i] = varval[i].replace("${unavailable}", s)
-    elif "${unavailable}" in a[i]:
-        varval[i] = varval[i].replace("${unavailable}", s)
-        return back_prop(a, i - 1, s, c) + 1
-    return 0
-
+        varval[i] = varval[i].replace("${tape}",s)
+    elif "${tape}" in a[i]:
+        varval[i] = varval[i].replace("${tape}",s)
+        back_prop(a,i-1,s)
 
 with open("cfstore/json/tapefiles.json", "w") as f:
     json.dump(replacementfiles, f)
@@ -38,5 +36,12 @@ for varkey, varval in growncfa.variables.items():
             k += 1
             val = varval[v]
             if val in tapefiles:
-                c = back_prop(varval, v, tapefiles[val], 0)
+                back_prop(varval, v, tapefiles[val])
         varval = np.reshape(varval, varshape)
+        growncfa.variables[varkey][...,0] = varval
+
+for varkey, varval in growncfa.variables.items():
+    if varkey.startswith("cfa_file"):
+        varshape = varval[..., 0].shape
+        varval = varval[..., 0].flatten().tolist()
+        print(varval[:-5])
